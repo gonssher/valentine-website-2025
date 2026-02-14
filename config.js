11ef510fe5a267ae17db9794a455d6ffe760c372
Sherwin Gonsalves <52938571+gonssher@users.noreply.github.com>
2026-02-14 07:42:07 -0500

    Update config.js

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ const CONFIG = {
         musicUrl: "https://res.cloudinary.com/doywpqimw/video/upload/v1771072517/Love_Story_by_Indila_Ethereal_Instrumental_Full_Version_mqugsz.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 1.5                        // Volume level (0.0 to 1.0)
+        volume: 0.5                        // Volume level (0.0 to 1.0)
     }
 };
 
